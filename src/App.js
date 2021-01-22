62470e425cd10f3cd0af9bee663847dab3cd48a9
Holly <milstowers@gmail.com>
2021-01-22 12:43:07 +0000

    FIle name update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer"
-import Main_content from "./components/Main_Content"
-import Main_head from "./components/Main_Head"
+import Main_Content from "./components/Main_Content"
+import Main_Head from "./components/Main_Head"
 import Nav from "./components/Nav"
 
 function App() {
