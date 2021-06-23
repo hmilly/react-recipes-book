b5bd1b6d8d64c723497bcb2875dc581fa403d90a
hmilly <milstowers@gmail.com>
2021-06-23 19:43:41 +0100

    updated img w/h

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,9 +4,7 @@ function Menu({ Burgers }) {
     const [allBurgers, setAllBurgers] = useState([])
     const [num, setnum] = useState(9)
     const [btn, setbtn] = useState(true)
-
     // json-server -p 8080 ./public/db.json
-
     useEffect(() => {
         const getBurgers = async () => {
             await fetch('db.json')
@@ -21,13 +19,13 @@ function Menu({ Burgers }) {
         setbtn(!btn)
         num === 12 ? setnum(9) : setnum(12)
     }
-
+    
     return (
         <div className="menu">
             <div className="menu_top">
                 <h1>Explore our Menus</h1>
                 <p>Choose from an ever-changing mix of meat, fish, Beyond Meat™, WW
-              Approved, Diabetes Friendly recipes and health-conscious offerings.</p>
+                    Approved, Diabetes Friendly recipes and health-conscious offerings.</p>
             </div>
             <div className="menu_mid">
                 {allBurgers.slice(0, num).map((b, i) => (
