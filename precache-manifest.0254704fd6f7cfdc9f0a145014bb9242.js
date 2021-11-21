905f55746d1435effbee3d1f7d22a1ce1db78da0
Holly <milstowers@gmail.com>
2021-11-21 11:21:26 +0000

    Updates

---
+++ precache-manifest.0254704fd6f7cfdc9f0a145014bb9242.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82926296f20ef38c260ff9c27898cfad",
+    "url": "/recipes-book/index.html"
+  },
+  {
+    "revision": "e62a038e162f0c9a494b",
+    "url": "/recipes-book/static/css/main.34cd2547.chunk.css"
+  },
+  {
+    "revision": "da69ae91de8dacd258a3",
+    "url": "/recipes-book/static/js/2.4e1ab2db.chunk.js"
+  },
+  {
+    "revision": "a64acf8e3ad6aa1497056f0bb9a7ce4f",
+    "url": "/recipes-book/static/js/2.4e1ab2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e62a038e162f0c9a494b",
+    "url": "/recipes-book/static/js/main.7c050582.chunk.js"
+  },
+  {
+    "revision": "f00a0e8c0a474d493be8",
+    "url": "/recipes-book/static/js/runtime-main.f075f8b2.js"
+  },
+  {
+    "revision": "29067540f28a2a515c6eac7269ff74ee",
+    "url": "/recipes-book/static/media/burger_component_img1.29067540.png"
+  },
+  {
+    "revision": "499c09eae0a9851be47e79ea94fb6537",
+    "url": "/recipes-book/static/media/complete_order.499c09ea.png"
+  },
+  {
+    "revision": "d648d378dae9a19e8f23ef9503446ccc",
+    "url": "/recipes-book/static/media/footer.d648d378.png"
+  },
+  {
+    "revision": "dce388a5617dad6c01797f7e5597b9dc",
+    "url": "/recipes-book/static/media/mainbot.dce388a5.png"
+  },
+  {
+    "revision": "59e5339d1b4c79b7eaa3b06dffa48660",
+    "url": "/recipes-book/static/media/mainhead.59e5339d.png"
+  }
+]);
